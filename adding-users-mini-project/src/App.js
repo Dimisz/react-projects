b7d021635bf3f89ebe9513b17fd837d68d08b5de
Vladimir Solovyov <leilyngale@icloud.com>
2022-08-31 09:02:41 +0300

    Scaffolded the project

--- adding-users-mini-project/src/App.js
+++ adding-users-mini-project/src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from 'react';
-
-import AddUser from "./components/Users/AddUser";
-import UsersList from "./components/Users/UsersList";
+import AddUser from './components/Users/AddUser';
 
 const App = () => {
-  const [usersList, setUsersList] = useState([]);
-  const addUserHandler = (userName, userAge, userId) => {
-    setUsersList((prevUsersList) => {
-      return [...prevUsersList, {name: userName, age: userAge, id: Math.random().toString()}];
-    })
-  }
-
-  return (
-    <React.Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList}/>
-    </React.Fragment>
+  return(
+    <div>
+      <AddUser />
+    </div>
   );
 }
 
